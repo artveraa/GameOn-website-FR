ebc1306a809b1047d1daa95119f9e4e8c35c48e6
Arthur <arthurvera96@gmail.com>
2021-11-12 13:27:15 +0100

    Vérification formulaire fonctionnelle

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,16 +12,26 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const submitBtn = document.querySelector(".btn-submit");
+const closeBtn = document.querySelectorAll(".btn-close");
+const closeForm = document.querySelectorAll(".close");
 const validationMessage = document.querySelector(".formValidation");
-const closeForm = document.querySelector(".close");
 const form = document.querySelector('form[name="reserve"]');
 
 
 // fermer le formulaire
 
-closeForm.addEventListener('click', e => {
-  modalbg.style.display = "none";
-  validationMessage.style.display = "none";
+closeForm.forEach(close => {
+close.addEventListener('click', e => {
+        modalbg.style.display = "none";
+        validationMessage.style.display = "none";
+    })
+});
+
+closeBtn.forEach(closeBtn => {
+    closeBtn.addEventListener("click", e => {
+        modalbg.style.display = "none";
+        validationMessage.style.display = "none";
+    })
 });
 
 // launch modal event
@@ -29,7 +39,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
 //messages d'erreurs par champs
@@ -40,17 +50,30 @@ const mail = form.elements["email"];
 const date = form.elements["birthdate"];
 const tournois = form.elements["quantity"];
 const city = form.querySelector('input[name="location"]:checked');
-const allInput = form.querySelectorAll('input');
 
 
 submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  allInput.forEach(input => {
-    let error = input.closest(".formData").querySelector('.error');
-    if (!input.validity.valid){
-      error.classList.add('show');
+    e.preventDefault();
+
+    formData.forEach(formData => {
+        formData.querySelectorAll('input').forEach(input => {
+            const err = input.closest(".formData").querySelector(".error");
+            if (!input.validity.valid) {
+
+                    err.classList.add('show');
+
+            } else {
+
+                    err.classList.remove("show");
+
+            }
+        })
+    })
+
+    if (form.checkValidity()) {
+        modalbg.style.display = "none";
+        validationMessage.style.display = "block";
     }
-  })
 
 });
 
